62b24955e9f6aec8a144dfc0257b7f7ee3fb700e
Thomas Taylor <ttaylor@eagle.org>
2024-12-03 16:20:00 -0500

    Adding PasswordMaker Blazor app

---
+++ passwordmaker/app.js
@@ -0,0 +1,10 @@
+﻿function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(
+        function () {
+            console.log("Text copied to clipboard");
+        },
+        function (err) {
+            console.error("Could not copy text: ", err);
+        }
+    );
+}
