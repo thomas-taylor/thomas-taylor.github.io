4a76ad34ca67f99a96a32cb6268c3465bdbd5d2c
Thomas Taylor <ttaylor@eagle.org>
2024-12-12 04:58:09 -0500

    Publishing password maker

--- passwordmaker/app.js
+++ passwordmaker/app.js
@@ -1,18 +1,11 @@
-﻿function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(
-        function () {
-            console.log("Text copied to clipboard");
-        },
-        function (err) {
-            console.error("Could not copy text: ", err);
-        }
-    );
+﻿export function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
 }
 
-function setLocalStorageValue(key, value) {
-    localStorage.setItem(key, value);
-}
-
-function getLocalStorageValue(key) {
+export function getLocalStorageValue(key) {
     return localStorage.getItem(key);
+}
+
+export function setLocalStorageValue(key, value) {
+    localStorage.setItem(key, value);
 }
